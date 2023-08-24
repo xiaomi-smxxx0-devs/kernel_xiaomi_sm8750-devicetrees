b8a4592a1ba1d24458d1bff1e9826ac5c5fdfb84
Soumen Ghosh <quic_soumeng@quicinc.com>
2023-08-24 13:22:53 -0700

    ARM: dts: msm: Add devicetree changes for opensource project
    This chane will migragate all dts and dtsi file to
    opensourse project
    
    CRs-Fixed: 3583121
    Change-Id: I837ee52ce68ad23fd5bff8ed69684824c9bfe3f4
    Signed-off-by: Soumen Ghosh <quic_soumeng@quicinc.com>

---
+++ config/pineapple.mk
@@ -0,0 +1,5 @@
+dtbo-$(CONFIG_ARCH_PINEAPPLE)	:= pineapple-camera.dtbo
+dtbo-$(CONFIG_ARCH_PINEAPPLE)	+= pineapple-camera-v2.dtbo \
+									pineapple-camera-sensor-cdp.dtbo \
+									pineapple-camera-sensor-mtp.dtbo \
+									pineapple-camera-sensor-qrd.dtbo
