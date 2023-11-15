98659863abc117a33d7a8282ef427ee247c8d120
Atiya Kailany <quic_akailany@quicinc.com>
2023-11-15 15:22:59 -0800

    ARM: dts: msm: move hdk platform dtsi files to opensoure
    Move HDK platform dtsi files to appropriate project and adding
    required licensing.
    
    CRs-Fixed: 3599138
    Change-Id: I35172180a9ce45cdc74b46fd3a181a5a5664f6e7
    Signed-off-by: Atiya Kailany <quic_akailany@quicinc.com>

--- config/pineapple.mk
+++ config/pineapple.mk
@@ -2,4 +2,5 @@ dtbo-$(CONFIG_ARCH_PINEAPPLE)	:= pineapple-camera.dtbo
 dtbo-$(CONFIG_ARCH_PINEAPPLE)	+= pineapple-camera-v2.dtbo \
 									pineapple-camera-sensor-cdp.dtbo \
 									pineapple-camera-sensor-mtp.dtbo \
+									pineapple-camera-sensor-hdk.dtbo \
 									pineapple-camera-sensor-qrd.dtbo
