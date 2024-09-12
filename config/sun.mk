7c44e2a721626059380d2942421dae61a960c0e9
Yash Upadhyay <quic_yupadhya@quicinc.com>
2024-09-12 12:48:16 +0530

    ARM: dts: msm: Base device tree changes for Tuna
    This change adds support for Tuna in device tree.
    Change includes support for both Tuna and Ramos soc boards.
    
    CRs-Fixed: 3933582
    Change-Id: I696ed4ab182508f399fd2102626248ad10df112d

--- config/sun.mk
+++ config/sun.mk
@@ -3,3 +3,5 @@ dtbo-$(CONFIG_ARCH_SUN)   += sun-camera-sensor-mtp.dtbo \
 				sun-camera-sensor-rumi.dtbo \
 				sun-camera-sensor-cdp.dtbo  \
 				sun-camera-sensor-qrd.dtbo
+
+dtbo-$(CONFIG_ARCH_TUNA)  += tuna-camera.dtbo
