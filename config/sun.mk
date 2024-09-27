faca95135f1e41b999f324357a24e317db4c651f
Shadul Shaikh <quic_shaduls@quicinc.com>
2024-09-27 23:16:11 +0530

    ARM: dts: msm: Initial camera sensor dtsi files for Tuna
    Initial sensor dtsi files for Tuna MTP/CDP/RCM with:
    - Peripherals: CCI0/CCI1,
    - CSIPHY instances: 0 to 4,
    - Sensors: IMX766/IMX858/JN1 nodes,
    - ASC sensor: OV32C node,
    - TPG instances: 1 to 3,
    - MCLK/RESET GPIO pin controls
    
    CRs-Fixed: 3940535.
    
    Change-Id: Ie2536cbf26380c152300ce122865d71e86c40a66
    Signed-off-by: Shadul Shaikh <quic_shaduls@quicinc.com>

--- config/sun.mk
+++ config/sun.mk
@@ -5,3 +5,6 @@ dtbo-$(CONFIG_ARCH_SUN)   += sun-camera-sensor-mtp.dtbo \
 				sun-camera-sensor-qrd.dtbo
 
 dtbo-$(CONFIG_ARCH_TUNA)  += tuna-camera.dtbo
+
+dtbo-$(CONFIG_ARCH_TUNA)  += tuna-camera-sensor-mtp.dtbo \
+				tuna-camera-sensor-cdp.dtbo
